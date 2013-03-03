9b892b33978e0cc2207f387d432c6d07cd42c9a7
Manoj Mathai <manojpmathai@gmail.com>
2013-03-03 18:57:42 +0530

    Solved #83

---
+++ src/foreclojure_solutions/p83.clj
@@ -0,0 +1,2 @@
+;Write a function which takes a variable number of booleans. Your function should return true if some of the parameters are true, but not all of the parameters are true. Otherwise your function should return false.
+#(< 0 (count (filter true? %&)) (count %&))
