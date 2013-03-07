2388cda66ba46d2baee1c8cf133ce0604c1d1e3a
Manoj Mathai <manojpmathai@gmail.com>
2013-03-08 00:27:06 +0530

    Solved #97

---
+++ src/foreclojure_solutions/p97.clj
@@ -0,0 +1,14 @@
+;Write a function which returns the nth row of Pascal's Triangle. 
+(fn prow [n]
+  (letfn [(pval [r, c]
+            (if (or (<= r 1) (= r c) (zero? c))
+              1
+              (+
+               (p (dec r) (dec c))
+               (p (dec r) c))))]
+    (map #(pval (dec n) %) (range n))))
+
+(fn prow [n]
+  (if (= 1 n)
+    [1]
+    `[1 ~@(map #(apply + %) (partition 2 1 (prow (dec n)))) 1]))
