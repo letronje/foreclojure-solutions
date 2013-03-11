3604981966965f17df2f4fde410c37bfb485b587
Manoj Mathai <manoj@yournextleap.com>
2013-03-11 15:38:03 +0530

    Added problem statement to #135

--- src/foreclojure_solutions/p135.clj
+++ src/foreclojure_solutions/p135.clj
@@ -1,3 +1,4 @@
+;Write a function that accepts a variable length mathematical expression consisting of numbers and the operations +, -, *, and /. Assume a simple calculator that does not do precedence and instead just calculates left to right.
 (fn infix [v & rem]
   (reduce (fn [v1, [f v2]]
             (f v1 v2))
